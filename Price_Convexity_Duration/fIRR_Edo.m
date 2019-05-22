1dcd1e8243ca0df657b9ebb213fec3e3da5414a1
Edoardo Busetti <EdoardoBusetti95@gmail.com>
2019-05-22 15:34:13 +0200

    Add files via upload

---
+++ Price_Convexity_Duration/fIRR_Edo.m
@@ -0,0 +1,28 @@
+function TIR_result = fIRR_Edo(Price,CashFlows,Maturities)
+
+%% %Example
+% Price  = 102.20; 
+% CashFlows = [6 6 106];
+% Maturities = [0.47 1.47 2.47];
+% TIR_result = tir_Edo(Price,CashFlows,Maturities)
+
+
+%{ 
+time = zeros(1,length(Maturities));
+for i = 1:length(Maturities)
+    time(i) = (Maturities(i) + Maturities(i)^2);
+end
+%}
+
+syms TIR
+
+DiscountFactors = ((1+TIR).^Maturities).^-1;
+
+DiscountedCF = CashFlows.*DiscountFactors;
+
+TIR_Eqn = sum(DiscountedCF) == Price;
+
+s = vpasolve(TIR_Eqn, TIR);
+
+TIR_result_vec = double(s);
+TIR_result = min(abs(real(TIR_result_vec)));
